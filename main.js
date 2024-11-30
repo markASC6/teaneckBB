a1804dbcbdff7f34e884146f89cd0d7a518d45ed
markASC6 <52749178+markASC6@users.noreply.github.com>
2024-11-30 11:45:09 -0500

    Works

--- main.js
+++ main.js
@@ -80,37 +80,55 @@ fetchQuestions(EXODUS_CSV, EXODUS);
 // User picking question bank
 
 let questionBank = [];  // the set of questions that are actually going to be shown
-
-let started = false;    // has the user hit the start button
-
-let currQuestion = 0;
+let questionStock = []; // copy of question bank that will remain unchanged for question drops
 
 class Sequence {
     constructor(){
         this.length;
         this.curr = 0;
+        this.seq = [];          // in order sequence
         this.shuffled = [];     // shuffled version of the sequence
         this.isShuffled = false;
+        this.isFinished = false;
     }
     setLength(n){
         this.length = n;
+        this.seq = [];
+        // Create Sequence of length n
+        for (let i = 0; i < this.length; i++){
+            this.seq.push(i);
+        }
     }
     setCurr(num){
         this.curr = num;
     }
+    left(){
+        // if the first card
+        if (this.curr <= 0) return false;
+        this.curr--;
+        return true;
+    }
+    right(){
+        // if not the last card
+        if (this.curr < this.length - 1){
+            this.curr++;
+            return true;
+        }
+        else {
+            Seq.isFinished = true;
+            return false;
+        }
+    }
     shuffle(){
-        this.bank = [];
         this.shuffled = [];
         this.curr = 0;
         // [0, 1, 2, ...]
-        for (let i = 0; i < this.length; i++){
-            this.bank.push(i);
-        }
+        let bank = this.seq.slice();
         // get random number from bank without replacement until there's nothing left
-        while (this.bank.length > 0){
-            let aRand = Math.floor(Math.random()*this.bank.length);
-            this.shuffled.push(this.bank[aRand]);
-            this.bank.splice(aRand, 1); // remove aRand from bank
+        while (bank.length > 0){
+            let aRand = Math.floor(Math.random()*bank.length);
+            this.shuffled.push(bank[aRand]);
+            bank.splice(aRand, 1); // remove aRand from bank
         }
         this.isShuffled = true;
     }
@@ -118,42 +136,38 @@ class Sequence {
         this.curr = 0;
         this.shuffled = [];
         this.isShuffled = false;
+        this.isFinished = false;
     }
     // getter for this.curr that factors in shuffling
     i(){
         if (this.isShuffled) return this.shuffled[this.curr];
-        return this.curr;
+        return this.seq[this.curr];
+    }
+    dropCurr(){
+        // if dropping the last question
+        if (this.length <= 1){
+            this.isFinished = true;
+            return;
+        }
+        this.length--;
+        if (this.isShuffled){
+            // remove from unshuffled version
+            let shuffledVal = this.shuffled[this.curr];
+            let seqIndex = this.seq.indexOf(shuffledVal);
+            if (seqIndex != -1) this.seq.splice(seqIndex, 1);
+            // remove from shuffled
+            this.shuffled.splice(this.curr, 1);
+        }
+        else {
+            this.seq.splice(this.curr, 1);
+        }
+        // if you dropped the last card
+        if (this.curr == this.length) this.curr--;
     }
 }
 
 Seq = new Sequence();
 
-
-// Note card span elements
-const question = document.getElementById('q_text');
-const answer = document.getElementById('a_text');
-const infoText = document.getElementById('infoText')
-
-function start(){
-    started = true;
-
-    let isExodus = document.getElementById('book1').checked;
-    let exodusMin = parseInt(document.getElementById('book1min').value);
-    let exodusMax = parseInt(document.getElementById('book1max').value);
-    console.log(isExodus, exodusMin, exodusMax)
-
-    // Add question set questions to question bank
-    for (let i = exodusMin - 1; i <= exodusMax - 1; i++){
-        questionBank = questionBank.concat(questionSet[EXODUS][i])
-    }
-
-    Seq.setLength(questionBank.length);
-    Seq.setCurr(0);
-    question.textContent = questionBank[Seq.i()][Q];
-    answer.textContent = "";
-    
-    infoText.textContent = "Question 1 of " + questionBank.length;
-}
 
 // User controls
 
@@ -162,34 +176,87 @@ class Controls {
         this.question = document.getElementById('q_text');
         this.answer = document.getElementById('a_text');
         this.infoText = document.getElementById('infoText');
-        this.shuffleButton = document.getElementById('shuffle_button')
+        this.shuffleButton = document.getElementById('shuffle_button');
+        this.blank = document.getElementById('blank');
+        this.finishText = document.getElementById('finishText');
+        
+        this.started = false;
+        this.isFinished = false;
+        this.dropCount = 0;
     }
+    
+    
+    start(){
+        this.started = true;
+        questionBank = [];
+    
+        // Get user info
+        let isExodus = document.getElementById('book1').checked;
+        let exodusMin = parseInt(document.getElementById('book1min').value);
+        let exodusMax = parseInt(document.getElementById('book1max').value);
+        console.log(isExodus, exodusMin, exodusMax)
+    
+        // Move specified questions from question set to question bank
+        for (let i = exodusMin - 1; i <= exodusMax - 1; i++){
+            questionBank = questionBank.concat(questionSet[EXODUS][i]);
+        }
+        questionStock = questionBank.slice();    // shallow copy
+    
+        this.isFinished = false;
+        let isHidden = notecard.classList[0] == "hide";     // to-do: make this a class var
+        if (isHidden) notecard.classList.toggle("hide");    // take away .hide
+        
+        this.shuffleButton.textContent = "Shuffle";
+        if (this.shuffleButton.classList[0] == "clicked"){
+            this.shuffleButton.classList.toggle("clicked");
+        }
+        
+        Seq.unshuffle();     // resets Seq
+        Seq.setLength(questionBank.length);
+        this.dropCount = 0;
 
+        this.showQ();
+    }
+    
     left(){
-        if (!started) return;
+        if (!this.started || this.isFinished) return;
 
-        if (Seq.curr > 0) Seq.curr--;
-        this.question.textContent = questionBank[Seq.i()][Q];
-        this.answer.textContent = "";
-
-        this.infoText.textContent = `Question ${Seq.curr + 1} of ${Seq.length}`
+        // if there is a change, show it
+        if (Seq.left()) this.showQ();
     }
     right(){
-        if (!started) return;
+        if (!this.started || this.isFinished) return;
 
-        if (Seq.curr < questionBank.length - 1) Seq.curr++;
-        this.question.textContent = questionBank[Seq.i()][Q];
+        if (Seq.right()){
+            this.showQ();
+            return;
+        }
+        if (Seq.isFinished){
+            if (!this.isFinished) this.finished();
+            return;
+        }
+
+        // if (Seq.curr >= questionBank.length - 1){
+        //     if (!this.isFinished) this.finished();
+        //     return;
+        // }
+        // if (Seq.curr < questionBank.length - 1) Seq.curr++;
+        // this.showQ()
+    }
+    showQ(){
+        this.question.textContent = capitalizeFirstLetter(questionBank[Seq.i()][Q]);
         this.answer.textContent = "";
 
         this.infoText.textContent = `Question ${Seq.curr + 1} of ${Seq.length}`
+
     }
     space(){
-        if (!started) return;
+        if (!this.started) return;
 
-        answer.textContent = capitalizeFirstLetter(questionBank[Seq.i()][A]);
+        this.answer.textContent = capitalizeFirstLetter(questionBank[Seq.i()][A]);
     }
     shuffle(){
-        if (!started) return;
+        if (!this.started) return;
 
         if (this.shuffleButton.textContent.toLowerCase() == "shuffle"){
             Seq.shuffle();
@@ -197,7 +264,7 @@ class Controls {
             // this.shuffleButton.style.backgroundColor = "#7d2505";
             this.shuffleButton.classList.toggle("clicked");
 
-            this.question.textContent = questionBank[Seq.i()][Q];
+            this.question.textContent = capitalizeFirstLetter(questionBank[Seq.i()][Q]);
             this.answer.textContent = "";
 
             this.infoText.textContent = `Question ${Seq.curr + 1} of ${Seq.length}`
@@ -205,10 +272,9 @@ class Controls {
         else if (this.shuffleButton.textContent.toLowerCase() == "unshuffle"){
             Seq.unshuffle();
             this.shuffleButton.textContent = "Shuffle";
-            // this.shuffleButton.style.backgroundColor = "#972D07";
             this.shuffleButton.classList.toggle("clicked");
 
-            this.question.textContent = questionBank[Seq.i()][Q];
+            this.question.textContent = capitalizeFirstLetter(questionBank[Seq.i()][Q]);
             this.answer.textContent = "";
 
             this.infoText.textContent = `Question ${Seq.curr + 1} of ${Seq.length}`
@@ -216,6 +282,83 @@ class Controls {
         else {
             console.log("Shuffling error")
         }
+    }
+    drop(){
+        if (!this.started) return;
+
+        Seq.dropCurr();
+        if (Seq.isFinished){
+            if (this.isFinished) this.finished();
+            return;
+        }
+        if (questionBank.length == 1){
+            if (!this.isFinished) this.finished();
+            return;
+        }
+        this.dropCount++;
+
+        // update info text
+        this.infoText.textContent = `Question ${Seq.curr + 1} of ${Seq.length}`
+
+
+        // fade animation
+        this.blank.classList.remove("hide");
+        this.blank.classList.add("fade_in_out")
+        // fade out .5 seconds
+        setTimeout(() => {
+            this.question.textContent = capitalizeFirstLetter(questionBank[Seq.i()][Q]);
+            this.answer.textContent = "";
+        }, 250)
+        setTimeout(() => {
+            this.blank.classList.add("hide")
+            this.blank.classList.remove("fade_in_out")
+        }, 500)
+        // fade in .5 seconds
+
+    }
+    finished(){
+        this.isFinished = true;
+        let isHidden = (notecard.classList[0] == "hide");
+        if (!isHidden) notecard.classList.toggle("hide");
+
+        this.finishText.innerHTML = `
+            You've completed this question set!
+            <br>Questions reviewed: ${questionStock.length}
+            <br>Questions dropped: ${this.dropCount}
+            <br> Would you like to
+            <br>&emsp; Review the undropped questions?
+            <br>&emsp; Review all the questions?
+            <br>&emsp; Change the chapters at the top?
+        `;
+    }
+    reUndropped(){
+        if (!this.started || !this.isFinished) return;
+        this.isFinished = false;
+        let isHidden = notecard.classList[0] == "hide";
+        if (isHidden) notecard.classList.toggle("hide");    // take away .hide
+
+        Seq.unshuffle();
+        this.shuffleButton.textContent = "Shuffle";
+        if (this.shuffleButton.classList[0] == "clicked"){
+            this.shuffleButton.classList.toggle("clicked");
+        }
+        this.showQ();
+    }
+    reAll(){
+        if (!this.started || !this.isFinished) return;
+        this.isFinished = false;
+        let isHidden = notecard.classList[0] == "hide";
+        if (isHidden) notecard.classList.toggle("hide");    // take away .hide
+
+        questionBank = questionStock.slice();   // copy of questionStock
+        Seq.unshuffle();
+        this.shuffleButton.textContent = "Shuffle";
+        if (this.shuffleButton.classList[0] == "clicked"){
+            this.shuffleButton.classList.toggle("clicked");
+        }
+        Seq.setLength(questionBank.length);
+        this.dropCount = 0;
+        this.showQ();
     }
 
 }
@@ -237,7 +380,9 @@ document.addEventListener('keydown', function(event) {
     }
     if (event.code == "KeyH"){
         Ctrl.shuffle();
-        console.log("H");
+    }
+    if (event.code == "KeyD"){
+        Ctrl.drop();
     }
 });
 
